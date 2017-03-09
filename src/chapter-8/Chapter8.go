97280344fede37f50d534391728cfc38efe3a8a9
Jarven <769558579@qq.com>
2017-03-09 16:56:32 +0800

    内存分配

--- src/chapter-8/Chapter8.go
+++ src/chapter-8/Chapter8.go
@@ -1,1 +1,24 @@
-package chapter_8
+package main;
+
+import (
+    "fmt"
+)
+
+func main() {
+
+    // p 为指向 int 类型的指针,并且值为nil,也就是null值 又称为空指针
+    var p *int;
+    fmt.Println(p)
+
+    // &  为取址操作符  整体含义为  定义i为整形变量,并定义j为指向i地址的数据
+    var i_1 int = 15;
+    var j = &i_1;
+    fmt.Println(i_1)
+    fmt.Printf("%d\n",j)
+    for i:=0;i<10 ;i++  {
+        i_1++;
+    }
+    fmt.Println(i_1)
+    fmt.Println(j)
+
+}
